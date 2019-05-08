3020f5e0a585c244587d81fc47b71131c8cb1a5b
alexander07in <alexander07in@gmail.com>
2019-05-07 23:38:27 -0500

    Subiendo Practica 03 MVC

---
+++ Practica 03/Practica03LabIngSw2/src/client/Main.java
@@ -0,0 +1,13 @@
+package client;
+
+/**
+ * Integrantes: Hurtado Pedraza Gisel
+ *              Inagan Pincho Alexander
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        RunMVC mainRunMVC = new RunMVC();
+    }
+
+}
